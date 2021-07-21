3c337965742735fc7f46f721887fbe1c80b2eb7a
Kurt Snuggly <unsnugHero@gmail.com>
2021-07-20 23:52:09 -0400

    delete request

--- server/src/controllers/request.controller.ts
+++ server/src/controllers/request.controller.ts
@@ -1,15 +1,16 @@
 import { NextFunction, Request, Response } from 'express';
-import { ObjectId } from 'mongoose';
+
 import { CustomError } from 'util/helpers';
 
 import { Request as PhanRequest } from '../models/Request';
 
 class RequestController {
   public async getRequest(req: Request, res: Response, next: NextFunction) {
-    const requestId: ObjectId = req.body?.requestId;
+    const requestId: string = req.params.requestId;
 
     try {
       const phanRequest = await PhanRequest.findById(requestId);
+
       return res.status(200).json(phanRequest);
     } catch (error) {
       if (error.kind === 'ObjectId') {
@@ -23,7 +24,18 @@ class RequestController {
 
   public async updateRequest(req: Request, res: Response, next: NextFunction) {}
 
-  public async deleteRequest(req: Request, res: Response, next: NextFunction) {}
+  public async deleteRequest(req: Request, res: Response, next: NextFunction) {
+    const requestId: string = req.params.requestId;
+
+    try {
+      await PhanRequest.findByIdAndDelete(requestId);
+    } catch (error) {
+      if (error.kind === 'ObjectId') {
+        return res.status(404).json({ msg: 'Post not found' });
+      }
+      next(error);
+    }
+  }
 }
 
 export = new RequestController();
