300dc57e73da4ac5f2ae371bc2bd4ac4bc04c8e4
Kurt Snuggly <unsnugHero@gmail.com>
2021-07-25 00:31:59 -0400

    improve request update validators

--- server/src/middleware/validators/request.validators.ts
+++ server/src/middleware/validators/request.validators.ts
@@ -1,11 +1,12 @@
+import { Request } from 'express';
 import { check } from 'express-validator';
 
-import { Request } from '../../models/Request';
+import { Request as PhanRequest } from '../../models/Request';
 
 // Create Request Validators & Helpers
 
 const checkDuplicateRequestSubject = async (subject: string) => {
-  const request = await Request.findOne({ subject });
+  const request = await PhanRequest.findOne({ subject });
   if (request) return Promise.reject();
 };
 
@@ -16,8 +17,18 @@ export const createRequestValidators = [
   )
 ];
 
-// Update Request Validators
+// Update Request Validators & Helpers
+
+// want to check that subject is not a duplicate only if the matching request found is not the one being updated
+const checkDuplicateRequestSubjectUpdate = async (subject: string, req: Request) => {
+  const requestId = req.params.requestId;
+  const request = await PhanRequest.findOne({ subject });
+  if (request && requestId !== request.id) return Promise.reject();
+};
 
 export const updateRequestValidators = [
-  check(['subject', 'description', 'location'], 'Field cannot be empty').notEmpty()
+  check(['subject', 'description', 'location'], 'Field cannot be empty').notEmpty(),
+  check('subject', 'A Request with this subject already exists').custom((subject, { req }) =>
+    checkDuplicateRequestSubjectUpdate(subject, req as Request)
+  )
 ];
