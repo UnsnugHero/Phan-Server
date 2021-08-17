3cd10ad952308cc0ef7b27619d1f0d26ba2b7eaf
Kurt Snuggly <unsnugHero@gmail.com>
2021-08-16 23:11:35 -0400

    missed the routes

--- src/routers/poll.router.ts
+++ src/routers/poll.router.ts
@@ -19,6 +19,8 @@ class PollRouter {
     this._router.get('/', this._controller.getCurrentPoll);
     this._router.post('/create', [authToken], this._controller.createNewPoll);
     this._router.put('/archive/:pollId', [authToken], this._controller.archivePoll);
+    this._router.put('/vote/yes/:pollId', [authToken], this._controller.voteYesPoll);
+    this._router.put('/vote/no/:pollId', [authToken], this._controller.voteNoPoll);
   }
 }
 
